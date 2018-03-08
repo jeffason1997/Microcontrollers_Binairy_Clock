4446240f5a52068a57672357268268fed42d6f14
jeffason1997 <jeffreylantinga@hotmail.com>
2018-03-08 14:38:41 +0100

    added project

---
+++ BinairyClock_Microcontrollers/BinairyClock_Microcontrollers/main.c
@@ -0,0 +1,18 @@
+/*
+ * BinairyClock_Microcontrollers.c
+ *
+ * Created: 8-3-2018 14:37:26
+ * Author : jeffr
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
